869c4c58d9ff3ff700d4c55ccf5bc03beba482a0
unknown <adrian_salatino@hotmail.com>
2020-01-20 16:43:48 -0300

    validaciones js expresiones regulares hasta la mitad

--- js/validacion.js
+++ js/validacion.js
@@ -6,52 +6,65 @@ nombre = document.querySelector("input[name=Nombre]");
 console.log(nombre);
 apellido = document.querySelector("input[name=Apellido]");
 email = document.querySelector("input[name=Email]");
+console.log(email);
 telefono = document.querySelector("input[name=Telefono]");
 consulta = document.querySelector("input[name=Consulta]");
 dni = document.querySelector("input[name=DNI]");
 contraseña = document.querySelector("input[name=Contraseña]");
 repetirContraseña = document.querySelector("input[name=RepetirContraseña]");
-checkNum = /^([0-9])*$/;
+checkName = /^[^<>()[\].,;:\s@"]+(?:\.[^<>()[\].,;:\s@"]+)*@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i;
+checkNum = /([0-9])$/;
+checkLet = /([a-z])$/i;
 checkPass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
 expresionEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 
 nombre.onchange = function (){
-  console.log("estoy escribiendo");
   if (this.value.length >= 30 && this.lenght != ""){
     alert("Tu nombre debe de ser más corto");
+    this.value = "";
     event.preventDefault();
-  }else if (this.value != checkNum) {
-    alert(" No puedes incluir numeros en tu nombre")
+  }//else if (this.value != checkNum) {
+    else if (checkNum.test(this.value)) {
+    alert("No puedes incluir numeros en tu nombre");
+    this.value = "";
     event.preventDefault();
   }
+
 }
 apellido.onchange = function (){
-  if (this.lenght >= 30 && this.lenght != ""){
+  if (this.value.lenght >= 30 && this.lenght != ""){
     alert("Tu apellido debe de ser más corto")
     event.preventDefault();
   }
-  else if (this != checkNum) {
-    alert(" No puedes incluir numeros en tu apellido")
+  else if (checkNum.test(this.value)) {
+    alert(" No puedes incluir numeros en tu apellido");
+    this.value = "";
     event.preventDefault();
   }
 }
 email.onchange = function (){
-  if (this.lenght >= 100 && this.lenght != ""){
-    alert("Tu email debe de ser más corto")
+
+  if (this.lenght >= 3 && this.lenght != ""){
+    alert("Tu email debe de ser más corto");
+    this.value = "";
     event.preventDefault();
   }
-  else if (expresionEmail.test(this)) {
-    alert(" Tu email tiene un formato invalido ")
+  else if (!expresionEmail.test(this.value)) {
+    alert("Tu email tiene un formato invalido ");
+    console.log("test expresion = "+ expresionEmail.test(this.value));
+    this.value = "";
     event.preventDefault();
   }
 }
+
+//comprobar validaciones desde aca para abajo
 telefono.onchange = function (){
-  if (this.lenght >= 30 && this.lenght != ""){
+  if (this.value.lenght >= 30 && this.lenght != ""){
     alert("Tu teléfono debe de ser más corto")
     event.preventDefault();
   }
-  else if (checkNum.Test(this)) {
+  else if (checkLet.test(this)) {
     alert(" Tu teléfono debe contener solamente numeros")
     event.preventDefault();
   }
